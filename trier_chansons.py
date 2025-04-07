cde68ba46f71863069fb9b3e3f55698a230aba02
k4ctuus <louiskuyps@gmail.com>
2025-04-07 15:37:27 +0200

    Delete trier_chansons.py

--- trier_chansons.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('chansons.json', 'r') as file:
-    chansons = json.load(file)
-
-chansons_sorted = sorted(chansons, key=lambda x: x['titre'])
-
-with open('chansons_sorted.json', 'w') as file:
-    json.dump(chansons_sorted, file, indent=4)
